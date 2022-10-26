6cb79efc35bf8f2d44bce8354cde7e2033300b0f
Yuta Ohira <alesion90@gmail.com>
2022-10-26 21:57:18 +0900

    fetch info from mufj

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,36 @@
 import * as functions from "firebase-functions";
+import { launch } from "puppeteer";
 
-// Start writing Firebase Functions
-// https://firebase.google.com/docs/functions/typescript
+export const helloWorld = functions.https.onRequest(async (_, response) => {
+  try {
+    const browser = await launch();
+    const page = await browser.newPage();
+    await page.goto(
+      "https://fs.bk.mufg.jp/webasp/mufg/fund/detail/m03920420.html",
+    );
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+    // 基準価額
+    const basePriceSelector = "#kijyunKagaku";
+    await page.waitForSelector(basePriceSelector);
+    const basePrice = await page.evaluate((selector) => {
+      const el = document.querySelector(selector);
+      return el?.textContent;
+    }, basePriceSelector);
+    functions.logger.info(basePrice);
+
+    // 前日比
+    const dayChangeSelector = "#dayChange";
+    await page.waitForSelector(dayChangeSelector);
+    const dayChange = await page.evaluate((selector) => {
+      const el = document.querySelector(selector);
+      return el?.textContent;
+    }, dayChangeSelector);
+    functions.logger.info(dayChange);
+
+    await browser.close();
+    response.send("Hello from Firebase!");
+  } catch (err) {
+    functions.logger.error(`${err}`);
+    response.status(500).send("Error from Firebase!");
+  }
 });
