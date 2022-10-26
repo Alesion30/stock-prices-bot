b9221fdf60e9233f264e085190acdc0329f8ac0c
Yuta Ohira <alesion90@gmail.com>
2022-10-26 23:17:51 +0900

    cron

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,67 +3,67 @@ import { fetchForMufj } from "./utils/mufj";
 import { getNow } from "./services/dayjs";
 import { instance } from "./services/gaxios";
 
-export const helloWorld = functions.https.onRequest(async (_, response) => {
-  const now = getNow();
+export const scheduledPostToSlack = functions
+  .region("asia-northeast1")
+  .pubsub.schedule("0 12 * * *")
+  .onRun(async () => {
+    const now = getNow();
 
-  try {
-    const { name, basePrice, dayChange } = await fetchForMufj(
-      "allianceBernstein",
-    );
+    try {
+      const { name, basePrice, dayChange } = await fetchForMufj(
+        "allianceBernstein",
+      );
 
-    await instance.request({
-      method: "POST",
-      url: process.env.SLACK_WEBHOOK_URL,
-      data: {
-        blocks: [
-          {
-            type: "header",
-            text: {
-              type: "plain_text",
-              text: name,
-              emoji: true,
+      await instance.request({
+        method: "POST",
+        url: process.env.SLACK_WEBHOOK_URL,
+        data: {
+          blocks: [
+            {
+              type: "header",
+              text: {
+                type: "plain_text",
+                text: name,
+                emoji: true,
+              },
             },
-          },
-          {
-            type: "section",
-            fields: [
-              {
-                type: "mrkdwn",
-                text: `*基準価額:*\n${basePrice}円`,
-              },
-              {
-                type: "mrkdwn",
-                text: `*前日比:*\n${dayChange}`,
-              },
-            ],
-          },
-          {
-            type: "section",
-            fields: [
-              {
-                type: "mrkdwn",
-                text: `*日付:*\n${now.format("YYYY-MM-DD HH:mm:ss")}`,
-              },
-              {
-                type: "mrkdwn",
-                text: "*情報元:*\n<https://www.bk.mufg.jp/|三菱UFJ銀行>",
-              },
-            ],
-          },
-          {
-            type: "section",
-            text: {
-              type: "mrkdwn",
-              text: "<https://fs.bk.mufg.jp/webasp/mufg/fund/detail/m03920420.html|View more>",
+            {
+              type: "section",
+              fields: [
+                {
+                  type: "mrkdwn",
+                  text: `*基準価額:*\n${basePrice}円`,
+                },
+                {
+                  type: "mrkdwn",
+                  text: `*前日比:*\n${dayChange}`,
+                },
+              ],
             },
-          },
-        ],
-      },
-    });
-
-    response.send("Hello from Firebase!");
-  } catch (err) {
-    functions.logger.error(`${err}`);
-    response.status(500).send("Error from Firebase!");
-  }
-});
+            {
+              type: "section",
+              fields: [
+                {
+                  type: "mrkdwn",
+                  text: `*日付:*\n${now.format("YYYY-MM-DD HH:mm:ss")}`,
+                },
+                {
+                  type: "mrkdwn",
+                  text: "*情報元:*\n<https://www.bk.mufg.jp/|三菱UFJ銀行>",
+                },
+              ],
+            },
+            {
+              type: "section",
+              text: {
+                type: "mrkdwn",
+                text: "<https://fs.bk.mufg.jp/webasp/mufg/fund/detail/m03920420.html|View more>",
+              },
+            },
+          ],
+        },
+      });
+    } catch (err) {
+      functions.logger.error(`${err}`);
+    }
+  });
