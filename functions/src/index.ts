7ffcb001cd8e7eb3cadef662276f24bc2c05ee28
Yuta Ohira <alesion90@gmail.com>
2022-10-28 08:15:48 +0900

    fix cron

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -79,7 +79,7 @@ const runtimeOpts: functions.RuntimeOptions = {
 export const scheduledPostToSlack = functions
   .region("asia-northeast1")
   .runWith(runtimeOpts)
-  .pubsub.schedule("0 1-13/3 * * *") // 9時-21時で3時間おきに実行
+  .pubsub.schedule("0 17-5/3 * * *") // 9時-21時で3時間おきに実行
   .onRun(async () => {
     const browser = await launch();
     await postToSlackAllWorkspaces(browser);
